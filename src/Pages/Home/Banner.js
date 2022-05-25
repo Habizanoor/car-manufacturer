849d8bd840d50eaa08f5172b2cc3b4fc38063f93
Habizanoor <habizaboor.cse@gmail.com>
2022-05-25 13:28:45 +0600

    banner added

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -6,13 +6,17 @@ import banner3 from '../../images/banner/banner-3.jpg';
 const Banner = () => {
     return (
         <div className=' grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1  gap-4'>
-            <div className=''>
-                <img className='lg:h-96 h-24 lg:w-auto w-96' src={banner1} alt="" />
-                
+            <div className='relative'>
+                <img className=' lg:h-96 h-24 lg:w-auto w-96' src={banner1} alt="" />
+                <div className='absolute inset-0  hover:bg-violet-50 opacity-25 '>
+                    <h3 className='text-5xl font-bold'>We make all car parts</h3>
+                </div>
             </div>
-            <div className=''>
+            <div className='relative'>
                 <img className='lg:h-96 h-24 lg:w-auto w-96' src={banner2} alt="" />
-
+                <div className='absolute inset-0 hover:bg-violet-50 opacity-25 '>
+                    <h3 className='text-5xl font-bold'>We make car engines</h3>
+                </div>
             </div>
         </div>
     );
